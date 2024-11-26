aaefe4694c6b1cc20e5c07f218d0cc99daafa5ea
ch-iv <icheppy101@gmail.com>
2024-11-26 12:19:06 -0500

    Implement `start_game` use case

--- src/main/java/us/jonathans/interface_adapter/start_game/StartGamePresenter.java
+++ src/main/java/us/jonathans/interface_adapter/start_game/StartGamePresenter.java
@@ -2,14 +2,19 @@ package us.jonathans.interface_adapter.start_game;
 
 import us.jonathans.use_case.start_game.StartGameOutputBoundary;
 import us.jonathans.use_case.start_game.StartGameOutputData;
-import us.jonathans.view.StartGameTextView;
 
 public class StartGamePresenter implements StartGameOutputBoundary {
-    public void prepareSuccessView(StartGameOutputData startGameOutputData) {
-        StartGameTextView startGameTextView = new StartGameTextView();
-        startGameTextView.updateAndPresent(
-                StartGameViewModel.build(startGameOutputData)
-        );
+    private final StartGameViewModel startGameViewModel;
 
+    public StartGamePresenter(StartGameViewModel startGameViewModel) {
+        this.startGameViewModel = startGameViewModel;
+    }
+
+
+    public void prepareSuccessView(StartGameOutputData startGameOutputData) {
+        startGameViewModel.setState(
+                StartGameState.build(startGameOutputData)
+        );
+        startGameViewModel.firePropertyChanged();
     }
 }
