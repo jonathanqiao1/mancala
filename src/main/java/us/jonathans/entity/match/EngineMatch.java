83545c926a077670049a0fd527a381041101c65c
icheppy101@gmail.com <alicesummers3000@gmail.com>
2024-11-26 11:32:44 -0500

    Create `EngineMatch` entity to encapsulate a match between a Human and an Engine

---
+++ src/main/java/us/jonathans/entity/match/EngineMatch.java
@@ -0,0 +1,20 @@
+package us.jonathans.entity.match;
+
+import us.jonathans.entity.rule.Game;
+
+import java.util.UUID;
+
+public class EngineMatch {
+    final UUID playerId;
+    final String engineId;
+    final Game game = new Game();
+
+    public EngineMatch(UUID playerId, String engineId) {
+        this.playerId = playerId;
+        this.engineId = engineId;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+}
