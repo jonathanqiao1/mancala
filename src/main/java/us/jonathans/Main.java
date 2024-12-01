24fac03a31791a7be599b70d5d4c9686fc4e78ac
RichardrLin <richardr.lin@mail.utoronto.ca>
2024-12-01 15:14:55 -0500

    Moved the NotifyUserUseCase into the appbuilder.

--- src/main/java/us/jonathans/Main.java
+++ src/main/java/us/jonathans/Main.java
@@ -11,6 +11,7 @@ public class Main {
                 .addMakePlayerMoveUseCase()
                 .addMakeComputerMoveUseCase()
                 .addCancelMatchUseCase()
+                .addNotifyUserUseCase()
                 .build();
         app.run();
     }
